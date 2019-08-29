407cf4e462b5feca3c7cb466be9f1bd2d57c860c
othmane <othman-19@hotmail.fr>
2019-08-29 15:21:53 +0100

    initial

--- db/migrate/20190828093527_add_attendee_id_to_attendances.rb
+++
@@ -1,5 +0,0 @@
-class AddAttendeeIdToAttendances < ActiveRecord::Migration[5.2]
-  def change
-    add_column :attendances, :attendee_id, :integer
-  end
-end
--- db/migrate/20190828093741_add_attended_event_id_to_attendances.rb
+++
@@ -1,5 +0,0 @@
-class AddAttendedEventIdToAttendances < ActiveRecord::Migration[5.2]
-  def change
-    add_column :attendances, :attended_event_id, :integer
-  end
-end
